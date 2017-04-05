57d235cd7c638c64090d314f71de6e16f6a5c539
addykumar27 <adarshna27@yahoo.com>
2017-04-05 09:12:12 -0700

    adding gifs

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,35 @@
-$(document).on("ready", function(){
+var giphy_api = "http://api.giphy.com/v1/gifs/search";
 
+$(document).on("ready", function(){
+  searchGifs();
+
+  $("form").on("submit", function(e) {
+    e.preventDefault();
+
+    searchGifs();
+  });
 });
+
+function searchGifs() {
+  $.ajax({
+    method: "GET",
+    url: "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC",
+    data: $("form").serialize(),
+    success: onSuccess,
+    error: onError
+  });
+}
+
+function onSuccess(json) {
+  $(".gif-img").remove();
+  json.data.forEach(function(data,i){
+    $(".gif-gallery").append($("<img class='gif-img' src="+data.images.fixed_height.url+">"));
+  });
+}
+
+function onError(xhr, status, errorThrown) {
+  alert("Sorry, there was a problem!");
+  console.log("Error: " + errorThrown);
+  console.log("Status: " + status);
+  console.dir(xhr);
+}
